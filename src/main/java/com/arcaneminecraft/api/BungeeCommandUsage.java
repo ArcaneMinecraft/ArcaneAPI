62b78c87a11458df53c229f124cc10c7739e72e7
Simon Chuu <simonorj@outlook.com>
2018-06-08 05:24:03 -0400

    Separate commands and usage help

--- src/main/java/com/arcaneminecraft/api/BungeeCommandUsage.java
+++ src/main/java/com/arcaneminecraft/api/BungeeCommandUsage.java
@@ -3,34 +3,49 @@ package com.arcaneminecraft.api;
 public enum BungeeCommandUsage {
     AFK             ("/afk"),
     APPLY           ("/apply"),
-    GREYLIST        ("/greylist", "arcane.command.greylist"),
+    GREYLIST        ("/greylist",   null,                   "arcane.command.greylist"),
+    REPLY           ("/reply",      "<private message ...>"),
     LINKS           ("/links"),
     NEWS            ("/news"),
-    PING            ("/ping [player]"),
-    SEEN            ("/seen <player>"),
-    FINDPLAYER      ("/findplayer <part of name>"),
-    FIRSTSEEN       ("/firstseen [player]"),
-    STAFFCHAT       ("/a <staff message ...>", "arcane.command.a"),
-    STAFFCHATTOGGLE ("/atoggle", "arcane.command.a");
+    PING            ("/ping",       "[player]"),
+    SEEN            ("/seen",       "<player>"),
+    SLAP            ("/slap",       "<player>",             "arcane.command.slap"),
+    FINDPLAYER      ("/findplayer", "<part of name>"),
+    FIRSTSEEN       ("/firstseen",  "[player]"),
+    STAFFCHAT       ("/a",          "<staff message ...>",  "arcane.command.a"),
+    STAFFCHATTOGGLE ("/atoggle",    null,                   "arcane.command.a");
 
+    private final String command;
     private final String usage;
     private final String permission;
 
-    BungeeCommandUsage(String usage){
-        this.usage = usage;
+    BungeeCommandUsage(String command){
+        this.command = command;
+        this.usage = command;
         this.permission = null;
     }
 
-    BungeeCommandUsage(String usage, String permission){
-        this.usage = usage;
+    BungeeCommandUsage(String command, String usage){
+        this.command = command;
+        this.usage = command + " " + usage;
+        this.permission = null;
+    }
+
+    BungeeCommandUsage(String command, String usage, String permission){
+        this.command = command;
+        this.usage = usage == null ? command : command + " " + usage;
         this.permission = permission;
     }
 
-    String getUsage() {
+    public String getCommand() {
+        return command;
+    }
+
+    public String getUsage() {
         return usage;
     }
 
-    String getPermission() {
+    public String getPermission() {
         return permission;
     }
 }
