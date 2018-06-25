c135cea2ca1da343c2c26b97abb144a249cf8fcf
Simon Chuu <simonorj@outlook.com>
2018-06-25 01:19:00 -0400

    Add aliases + add vanilla commands that Bungee plugin overrides

--- src/main/java/com/arcaneminecraft/api/BungeeCommandUsage.java
+++ src/main/java/com/arcaneminecraft/api/BungeeCommandUsage.java
@@ -1,74 +1,107 @@
 package com.arcaneminecraft.api;
 
 public enum BungeeCommandUsage {
-    AFK             ("afk"),
-    APPLY           ("apply"),
-    GREYLIST        ("greylist",    null,                   "arcane.command.greylist"),
-    REPLY           ("reply",       "<private message ...>"),
-    LINKS           ("links"),
-    ME              ("me",          "commands.me.usage"),
-    MSG             ("msg",         "commands.message.usage"),
+    APPLY           ("apply", null, "arcane.command.apply", "Apply for the greylist"),
+    BADGE           ("badge", "[badge name]", null, "Toggles or sets your tag", "b", "tag"),
+    BADGEADMIN      ("badgeadmin", "<>", "arcane.command.badgeadmin", "Administrative options for player tags"),
+    GREYLIST        ("greylist", null, "arcane.command.greylist"),
+    FINDPLAYER      ("findplayer", "<part of name>", null, "Find player name from part of name", "find"),
+    FIRSTSEEN       ("firstseen", "[player]", null, "Get the date when player first joined the game", "seenf"),
+    LINKS           ("links", null, "arcane.command.links", null, "link", "website", "forum", "discord"),
+    ME              ("me", "commands.me.usage"),
+    MSG             ("msg", "commands.message.usage", null, null, "tell", "m", "w"),
     NEWS            ("news"),
-    PING            ("ping",        "[player]"),
-    SEEN            ("seen",        "<player>"),
-    SLAP            ("slap",        "<player>",             "arcane.command.slap"),
-    FINDPLAYER      ("findplayer",  "<part of name>"),
-    FIRSTSEEN       ("firstseen",   "[player]"),
-    STAFFCHAT       ("a",           "<staff message ...>",  "arcane.command.a"),
-    STAFFCHATTOGGLE ("atoggle",     null,                   "arcane.command.a");
+    PING            ("ping", "[player]"),
+    REPLY           ("reply", "<private message ...>", null, null, "r"),
+    SEEN            ("seen", "<player>", null, "Get the date when player last joined the game"),
+    SLAP            ("slap", "<player>", "arcane.command.slap"),
+    STAFFCHAT       ("a", "<staff message ...>", "arcane.command.a"),
+    STAFFCHATTOGGLE ("atoggle", null, "arcane.command.a", null, "at");
 
     private final String name;
     private final String usage;
     private final String permission;
-    private final boolean vanillaNode;
+    private final String description;
+    private final String[] aliases;
 
-    BungeeCommandUsage(String name){
+    BungeeCommandUsage(String name) {
         this.name = name;
         this.usage = "/" + name;
         this.permission = null;
-        this.vanillaNode = false;
+        this.description = null;
+        this.aliases = new String[]{};
     }
 
-    BungeeCommandUsage(String name, String usage){
-        this.name = name;
-        if (usage.startsWith("commands.")) {
-            this.usage = usage;
-            this.vanillaNode = true;
-        } else {
-            this.usage = "/" + name + " " + usage;
-            this.vanillaNode = false;
-        }
-        this.permission = null;
+    BungeeCommandUsage(String name, String usage) {
+        this(name, usage, null, null);
     }
 
-    BungeeCommandUsage(String name, String usage, String permission){
+    BungeeCommandUsage(String name, String usage, String permission) {
+        this(name, usage, permission, null);
+    }
+
+    BungeeCommandUsage(String name, String usage, String permission, String description, String... aliases) {
         this.name = name;
-        if (usage != null && usage.startsWith("commands.")) {
-            this.usage = usage;
-            this.vanillaNode = true;
+        if (usage == null) {
+            this.usage = "/" + name;
         } else {
-            this.usage = "/" + name + (usage == null ? "" : " " + usage);
-            this.vanillaNode = false;
+            if (usage.startsWith("commands.")) {
+                this.usage = usage;
+            } else {
+                this.usage = "/" + name + " " + usage;
+            }
         }
         this.permission = permission;
+        this.description = description;
+        this.aliases = aliases;
     }
 
+    /**
+     * @return command name without slash
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return command with leading slash
+     */
     public String getCommand() {
         return "/" + name;
     }
 
+    /**
+     * @return usage with leading slash; Not Null
+     */
     public String getUsage() {
         return usage;
     }
 
-    public boolean usageIsVanillaNode() {
-        return vanillaNode;
+    /**
+     * @return command description; empty string if none
+     */
+    public String getDescription() {
+        return description == null ?  "" : description;
     }
 
+    /**
+     * @return command aliases in array.
+     */
+    public String[] getAliases() {
+        return aliases;
+    }
+
+    /**
+     * Checks if usage is a vanilla translatable node
+     * @return true if usage is a vanilla node
+     */
+    public boolean usageIsVanillaNode() {
+        return usage.startsWith("command.");
+    }
+
+    /**
+     * @return Returns permission node, null if none
+     */
     public String getPermission() {
         return permission;
     }
